985275eaef872356924dca074fd8680f34c08def
Fraciscv <Francisco.Andaur@icloud.com>
2020-06-10 10:04:13 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -21,8 +21,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -61,13 +59,6 @@ def dennis_ritchies_language
   end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# programmer_hash{:yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -91,9 +82,6 @@ def adding_matz
         end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -115,9 +103,6 @@ def changing_alan
 end
 
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
